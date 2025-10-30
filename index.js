88206d806f519846d760a4e554cfc518e7fe46e5
Shapel Nahyrych <shapel.nahyrych@gmail.com>
2025-10-30 09:06:17 +0000

    DATA Attributes

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const products = document.querySelectorAll("#shop .product");
 const check = document.querySelector("#shop h1 span");
 
+products.forEach((product)=>{
+  if(product.dataset.category === 'fruits&veget'){
+    product.style.backgroundColor = 'lightgreen'
+  }
+})
+
 let summa = 0;
 check.textContent = summa;
 
+const addToCart = (event) => {
+  
+  summa += Number(event.currentTarget.dataset.price);
+ 
+  if (summa > 150) {
+    alert('ви перевищили ліміт на картці')
+  event.currentTarget.removeEventListener("click", addToCart);
+  return;
+  }
+  event.currentTarget.style.backgroundColor = "pink";
+  check.textContent = summa;
+};
+
 products.forEach((product) => {
-  product.addEventListener("click", (event) => {
-    //event.currectTarget === product
-    event.currentTarget.style.backgroundColor = "pink";
-    summa += Number(event.currentTarget.dataset.price);
-    check.textContent = summa
-  });
+  product.addEventListener("click", addToCart);
 });
